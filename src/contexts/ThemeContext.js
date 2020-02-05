9ad8c12d36485794b9c3f0ffe1992f809ebaafbe
Julio <jusher22@gmail.com>
2020-02-05 14:59:39 -0600

    added theme context file

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react'
+
+export const ThemeContext = createContext()
+
+const ThemeContextProvider = () => {
+  return ( 
+    <div></div> 
+  );
+}
+ 
+export default ThemeContextProvider;

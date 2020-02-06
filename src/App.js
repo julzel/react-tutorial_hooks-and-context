02127820332ccef70f25bab9843ecb7ed1130a96
Julio <jusher22@gmail.com>
2020-02-06 17:58:43 -0600

    added theme context provider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './index.css';
 import Navbar from './components/Navbar';
 import Booklist from './components/Booklist';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Booklist />
+      <ThemeContextProvider>
+        <Navbar />
+        <Booklist />
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,11 +1,22 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const ThemeContext = createContext()
 
-const ThemeContextProvider = () => {
+const defaultThemeState = {
+  isLightTheme: true,
+  light: { textColor: '#555', ui: '#ddd', bg: '#eee' },
+  dark: { textColor: '#ddd', ui: '#333', bg: '#555' }
+}
+
+const ThemeContextProvider = ({ children }) => {
+
+  const [themeState] = useState(defaultThemeState)
+
   return ( 
-    <div></div> 
-  );
+    <ThemeContext.Provider value={{ ...themeState }}>
+      {children}
+    </ThemeContext.Provider> 
+  )
 }
  
-export default ThemeContextProvider;
+export default ThemeContextProvider
